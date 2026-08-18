f328c435254b6a5fec1cb9e4748ddf500ccfd1db
Emre Karakaya <e.k.karakaya3355@gmail.com>
2026-08-18 16:08:36 +0300

    Add train and inference module skeletons

--- app/inference.py
+++ app/inference.py
@@ -1,4 +1,27 @@
+import cv2
+from app.camera import Camera
+
+
 def run(configs, logger):
     logger.info("Çıkarım modu başlatıldı.")
-    logger.debug(f"Kullanılan config: {configs}")
-    print("[PREDICT] çıkarım burada çalışacak")
+
+    camera = Camera(**configs["camera"])
+    camera.open()
+    logger.info(f"Kamera Açıldı: {configs['camera']['source']}")
+
+    try:
+        while True:
+            ret, frame =camera.read()
+            if not ret:
+                logger.warning("Kare okunamadı, döngü sonlandırılıyor")
+                break
+            
+            cv2.imshow("Camera", frame)
+
+            if cv2.waitKey(1) & 0XFF == ord("q"):
+                logger.info("Kullanıcı çıkış yaptı")
+                break
+    finally:
+        camera.close()
+        logger.info("Kamera Kapatıldı")
+    
